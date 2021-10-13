21b94b6752ca0287cbdab59640d8211c9e4f2460
loqoman <14814137+loqoman@users.noreply.github.com>
2021-10-12 22:29:00 -0400

    :sparkles: Finishing primary features and helper methods in data analysis and adding a visuals directory

--- dataAnalysisPegasus.r
+++ dataAnalysisPegasus.r
@@ -1,4 +1,4 @@
-# Data Analysis on Pegasus Roastary EVN and Agtron converted values for the months of april through June.
+# agtronRawData Analysis on Pegasus Roastary EVN and Agtron converted values for the months of april through June.
 # By Darwin Clark for Pegasus Roastary
 
 # TODO: Indexing function for the xml (List the number of roasts, the date of the roast and the type of bean)
@@ -7,129 +7,161 @@ library(RColorBrewer)
 library(ggrepel)
 library(lubridate)
 
+library(argparse)
+
 library(xml2)
 library(purrr)
 
 library(plyr) # Used to fix the lists out of XML world
-library(dplyr) # Used to fix 
+library(dplyr) # Used to access group_by and get agtronRawDatabase statistics 
 
 # TODO: Rename this variable to somethnig meaningful
-data = read.csv("~/Programming/pegasusInternship2021/data/roastaryRoastData0603.csv")
-data$Date = as.Date(data$Date, format="%y.%m.%d") # Fix the dates in the d
- 
-xmlData = read_xml("~/Programming/pegasusInternship2021/data/For_darwin.xml")
+agtronRawData = read.csv("~/Programming/pegasusInternship2021/data/roastaryRoastData0819.csv")
+agtronRawData$Date = as.Date(agtronRawData$Date, format="%y.%m.%d") # Fix the dates in the d
+agtronRawData$Time = as.numeric(hm(agtronRawData$Time)) # Converts the character time's to numerical time's
+
+xmlagtronRawData = read_xml("~/Programming/pegasusInternship2021/data/For_darwin.xml")
 # Setting up colors
-myColors = brewer.pal("div")(5)
-show_col(brewer_pal()(10))
+# ====================== agtronRawData FORMATTING =========================== #
+countByBlend = agtronRawData %>% group_by(Blend) %>% summarize(count=n()) # Simple table with a count of each blend
 
-# ====================== DATA FORMATTING =========================== #
-
-names(myColors) = levels(data$Blend)
-custom_colors = scale_color_manual(name = "Blends", values = myColors)
-
-tempCountByBlend = data %>% group_by(Blend) %>% summarize(count=n())
-
+# ----- OPTIONAL, culling blends with less than a certain number of samples -----
+for(i in seq_len(nrow(countByBlend))){
+  # print(paste("This year is", df1$x[i], df1$y[i], "color", df1$z[i]))
+  if (countByBlend$count[i] < 15) {
+    agtronRawData = subset(agtronRawData, Blend != countByBlend$Blend[i])
+  }
+} 
 
 
 # ====================== START OF GRAPHING ========================== #
 # Boxplots
 
-# aes(fill = ifelse(tempCountByBlend[grep(data$Blend, tempCountByBlend$Blend),][[2]] > 15, "darkcyan", "red"))
+# aes(fill = ifelse(countByBlend[grep(agtronRawData$Blend, countByBlend$Blend),][[2]] > 15, "darkcyan", "red"))
 # TODO: Add coloring based off of sample size
-ggplot(data, aes(x = data$Blend, y = data$Agtron.Converted)) +
-  geom_boxplot(aes(color=data$Blend)) +
+ggplot(agtronRawData, aes(x = agtronRawData$Blend, y = agtronRawData$Agtron.Converted)) +
+  geom_boxplot(aes(color=agtronRawData$Blend)) +
   xlab("Blend Variety") + ylab("Agtron Converted Value") + labs(title = "Box Plots Comparing Agtron Converted Values of Different Roast Blends") +
   theme(legend.position = "non")
 
-summary(data$Agtron.Converted)
+summary(agtronRawData$Agtron.Converted)
 
-tempMaxDates = data %>% group_by(Blend) %>% filter(Date == max(Date))
+tempMaxDates = agtronRawData %>% group_by(Blend) %>% filter(Date == max(Date))
 
-# Scatter Plots
-ggplot(data, aes(x = data$Date, y = data$Agtron.Converted)) +
-  geom_line(aes(group=data$Blend, color=data$Blend), size = 1.1) +
-  #geom_point(aes(color=data$Blend)) +
-  geom_label_repel(aes(label=ifelse(Date>as.Date("2021-05-20"), Blend, ''), fill=data$Blend), nudge_x=1, na.rm = TRUE,
+# Labeled line plot 
+ggplot(agtronRawData, aes(x = agtronRawData$Date, y = agtronRawData$Agtron.Converted)) +
+  geom_line(aes(group=agtronRawData$Blend, color=agtronRawData$Blend), size = 1.1) +
+  #geom_point(aes(color=agtronRawData$Blend)) +
+  geom_label_repel(aes(label=ifelse(Date>as.Date("2021-05-20"), Blend, ''), fill=agtronRawData$Blend), nudge_x=1, na.rm = TRUE,
                    fontface = 'bold', color = 'white',
                    box.padding = unit(0.35, "lines"),
                    point.padding = unit(0.5, "lines"),
                    segment.color = 'grey50') +
   xlab("Date") + ylab("Agtron Converted Value") + labs(title = "Line Plot Comparing Date of Roast to Agtron Converted Value") 
 
-
-
-ggplot(data) +
-  geom_line(aes(x = data$Time, y = data$Agtron.Converted, group=data$Blend, color=data$Blend)) +
-  #geom_point(aes(color=data$Blend)) +
+  
+# Unlabeled line plot, colored by blend
+ggplot(agtronRawData) +
+  geom_line(aes(x = agtronRawData$Time, y = agtronRawData$Agtron.Converted, group=agtronRawData$Blend, color=agtronRawData$Blend), size = 1.5) +
+  #geom_point(aes(color=agtronRawData$Blend)) +
   xlab("Time") + ylab("Agtron Converted Value") + labs(title = "Line Plot Comparing Time of Roast to Agtron Converted Value") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, size=10))
 
 # ============================= XML 2 ============================= #
-masterXMLDataFrame = data.frame(matrix(vector(), 28, 3, dimnames=list(c(), c("Date", "File", "User"))), stringsAsFactors=F)
+masterXMLagtronRawDataFrame = data.frame(matrix(vector(), 28, 3)) #agtronRawData.frame(matrix(vector(), 28, 3, dimnames=list(c(), c("Date", "File", "User"))), stringsAsFactors=F)
 
-xml_attrs(xmlData, "RoastmasterEntities")
-  
-xml_children(xmlData)[2]["Roast"]
+xml_attrs(xmlagtronRawData, "RoastmasterEntities")
+    
+xml_children(xmlagtronRawData)[2]["Roast"]
 
-xml_find_all(xmlData, "RoastmasterXML/RoastmasterEntities/Roast")
+xml_find_all(xmlagtronRawData, "RoastmasterXML/RoastmasterEntities/Roast")
 
-# What I'll likely do is find all the roasts FIRST, then pull from the proper thermocouple in each roast to create a data.frame() object
-rawNodes = xml_find_all(xmlData, "//Node") # Will select every "Node" element in the Document
-rawRoasts = xml_find_all(xmlData, "//Roast") # Will select every "Roast" element in the Document
+# What I'll likely do is find all the roasts FIRST, then pull from the proper thermocouple in each roast to create a agtronRawData.frame() object
+rawNodes = xml_find_all(xmlagtronRawData, "//Node") # Will select every "Node" element in the Document
+rawRoasts = xml_find_all(xmlagtronRawData, "//Roast") # Will select every "Roast" element in the Document
 
-rawDataCurves = xml_find_all(xmlData, "//Curve") # Will select every "Curve" element in the Document
+rawagtronRawDataCurves = xml_find_all(xmlagtronRawData, "//Curve") # Will select every "Curve" element in the Document
 
 
-# ===== Testing how to get the first (of 4) "curve" datasets for each roast ===== # 
+# ===== Testing how to get the first (of 4) "curve" agtronRawDatasets for each roast ===== # 
 
-firstTestDataCurve = xml_find_all(xmlData, "//DataCurves")[1]
-secondTestDataCurve = xml_find_all(xmlData, "//DataCurves")[2]
+firstTestagtronRawDataCurve = xml_find_all(xmlagtronRawData, "//agtronRawDataCurves")[1]
+secondTestagtronRawDataCurve = xml_find_all(xmlagtronRawData, "//agtronRawDataCurves")[2]
 
 # Now we have the 4 'curve' objects fr each of the 2 roasts
 # now want to match for each of the thermocouples
 # N.B The matching is based off of a RegEX system called XPath
 
-xml_children(rawDataCurves)
-xml_attrs(rawDataCurves)
-xml_text(rawDataCurves)
+xml_children(rawagtronRawDataCurves)
+xml_attrs(rawagtronRawDataCurves)
+xml_text(rawagtronRawDataCurves)
 
 # These are each individual curve objects
-roast1AMBT = rawDataCurves[1] # Pick the first dataCurve (arbitraryily)
+roast1AMBT = rawagtronRawDataCurves[1] # Pick the first agtronRawDataCurve (arbitraryily)
 roast1AMBT = xml_find_all(roast1AMBT, ".//Node")  # Get all the "node" children within this roast 
 
-roast2AMBT = rawDataCurves[9] # Pick the NEXT AMBT dataCurve (arbitraryily)
+roast2AMBT = rawagtronRawDataCurves[9] # Pick the NEXT AMBT agtronRawDataCurve (arbitraryily)
 roast2AMBT = xml_find_all(roast2AMBT, ".//Node")
 
-roast1BT = rawDataCurves[2]
+roast1BT = rawagtronRawDataCurves[2]
 roast1BT = xml_find_all(roast1BT, ".//Node")  # Get all the "node" children within this roast 
   
-roast2BT = rawDataCurves[10]
+roast2BT = rawagtronRawDataCurves[10]
 roast2BT = xml_find_all(roast2BT, ".//Node")  # Get all the "node" children within this roast 
 
-masterXMLDataFrame$roast1dataCurve1Time = laply(laply(as_list(xml_find_all(roast1AMBT, ".//Time")),identity), identity) # REMEMBER SUBSET FUNCTION IS []
-masterXMLDataFrame$roast1dataCurve1Data = laply(laply(as_list(xml_find_all(roast1AMBT, ".//Level")), identity), identity) # REMEMBER SUBSET FUNCTION IS []
+masterXMLagtronRawDataFrame$roast1agtronRawDataCurve1Time = laply(laply(as_list(xml_find_all(roast1AMBT, ".//Time")),identity), identity) # REMEMBER SUBSET FUNCTION IS []
+masterXMLagtronRawDataFrame$roast1agtronRawDataCurve1agtronRawData = as.numeric(laply(laply(as_list(xml_find_all(roast1AMBT, ".//Level")), identity), identity)) # REMEMBER SUBSET FUNCTION IS []
 
-masterXMLDataFrame$roas2dataCurve1Data = laply(laply(as_list(xml_find_all(roast2AMBT, ".//Level")), identity), identity)[-c(29, 30)] # REMEMBER SUBSET FUNCTION IS []
+masterXMLagtronRawDataFrame$roas2agtronRawDataCurve1agtronRawData = as.numeric(laply(laply(as_list(xml_find_all(roast2AMBT, ".//Level")), identity), identity)[-c(29, 30)]) # REMEMBER SUBSET FUNCTION IS []
 
-masterXMLDataFrame$roast1BTData = laply(laply(as_list(xml_find_all(roast1BT, ".//Level")), identity), identity) # REMEMBER SUBSET FUNCTION IS []
-masterXMLDataFrame$roast2BTData = laply(laply(as_list(xml_find_all(roast2BT, ".//Level")), identity), identity)[-c(29, 30)] # REMEMBER SUBSET FUNCTION IS []
+masterXMLagtronRawDataFrame$roast1BTagtronRawData = as.numeric((laply(laply(as_list(xml_find_all(roast1BT, ".//Level")), identity), identity))) # REMEMBER SUBSET FUNCTION IS []
+masterXMLagtronRawDataFrame$roast2BTagtronRawData = as.numeric(laply(laply(as_list(xml_find_all(roast2BT, ".//Level")), identity), identity)[-c(29, 30)]) # REMEMBER SUBSET FUNCTION IS []
 
+# ----- Changing Time to mins in XML data frame -----
+masterXMLagtronRawDataFrame$roast1agtronRawDataCurve1Time = as.numeric(masterXMLagtronRawDataFrame$roast1agtronRawDataCurve1Time) / 60
 
 # RED = Curve 1 = r1 ambt
 # BLUE = Curve 2 = r1 Bt
-ggplot(masterXMLDataFrame, aes(x = masterXMLDataFrame$roast1dataCurve1Time, group=1)) +
-  geom_line(size=1.2, color="blue", aes(y= masterXMLDataFrame$roas2dataCurve1Data)) + 
-  geom_line(size=1.2, color="red", aes(y = masterXMLDataFrame$roast1dataCurve1Data)) +
-  xlab("Time (s) Since Start of Roast") + ylab("Ambient Temperature (Note flipped scales)") + labs(title = "Line Plot Comparing AMBT Thermocouple of Roast 1 (04.12 Peru)(Red) and Roast 2 (06.02 Peru)(Blue))") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggplot(masterXMLagtronRawDataFrame, aes(x = masterXMLagtronRawDataFrame$roast1agtronRawDataCurve1Time, group=1)) +
+  geom_line(size=1.2, color="blue", aes(y= masterXMLagtronRawDataFrame$roas2agtronRawDataCurve1agtronRawData)) + 
+  geom_line(size=1.2, color="red", aes(y = masterXMLagtronRawDataFrame$roast1agtronRawDataCurve1agtronRawData)) +
+  xlab("Time (s) Since Start of Roast") + ylab("Ambient Temperature (Note flipped scales)") + labs(title = "Line Plot Comparing AMBT Thermocouple of Roast 1 (04.12 Peru)(Red) and Roast 2 (06.02 Peru)(Blue)") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+  # + scale_y_discrete() + scale_y_reverse()
   
-ggplot(masterXMLDataFrame, aes(x = masterXMLDataFrame$roast1dataCurve1Time, group=1)) +
-  geom_line(size=1.2, color="blue", aes(y= masterXMLDataFrame$roast1BTData)) + 
-  geom_line(size=1.2, color="red", aes(y = masterXMLDataFrame$roast2BTData)) +
-  xlab("Time (s) Since Start of Roast") + ylab("BT Temperature") + labs(title = "Line Plot Comparing BT Thermocouple of Roast 1 (04.12 Peru)(Red) and Roast 2 (06.02 Peru)(Blue))") +
+ggplot(masterXMLagtronRawDataFrame, aes(x = masterXMLagtronRawDataFrame$roast1agtronRawDataCurve1Time, group=1)) +
+  geom_line(size=1.2, color="blue", aes(y= masterXMLagtronRawDataFrame$roast1BTagtronRawData)) + 
+  geom_line(size=1.2, color="red", aes(y = masterXMLagtronRawDataFrame$roast2BTagtronRawData)) +
+  
+  xlab("Time (s) Since Start of Roast") + ylab("BT Temperature") + labs(title = "Line Plot Comparing BT Thermocouple of Roast 1 (04.12 Peru)(Red) and Roast 2 (06.02 Peru)(Blue)") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  + scale_y_discrete()
 
-# roast1OXT = rawDataCurves[2]
+# ------ Derivitie -----
+for(i in seq_len(nrow(masterXMLagtronRawDataFrame) - 1)){
+  masterXMLagtronRawDataFrame$roast1BtDeriv[i] = (masterXMLagtronRawDataFrame$roast1BTagtronRawData[i+1] - masterXMLagtronRawDataFrame$roast1BTagtronRawData[i]) / 2
+  masterXMLagtronRawDataFrame$roast2BtDeriv[i] = (masterXMLagtronRawDataFrame$roast2BTagtronRawData[i+1] - masterXMLagtronRawDataFrame$roast2BTagtronRawData[i]) / 2
+} 
+
+ggplot(masterXMLagtronRawDataFrame, aes(x = masterXMLagtronRawDataFrame$roast1agtronRawDataCurve1Time, group=1)) +
+  #geom_line(size=1.2, color="blue", aes(y= masterXMLagtronRawDataFrame$roast1BTagtronRawData)) + 
+  #geom_line(size=1.2, color="red", aes(y = masterXMLagtronRawDataFrame$roast2BTagtronRawData)) +
+  geom_line(size=1.2, color="cyan", aes(y = masterXMLagtronRawDataFrame$roast1BtDeriv)) +
+  geom_line(size=1.2, color="orange", aes(y = masterXMLagtronRawDataFrame$roast2BtDeriv)) +
+  
+  xlab("Time (s) Since Start of Roast") + ylab("BT Temperature") + labs(title = "Line Plot Comparing BT Thermocouple of Roast 1 (04.12 Peru)(Red) and Roast 2 (06.02 Peru)(Blue)") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
++ scale_y_discrete()
+
+# ----- Faceting -----
+ggplot(agtronRawData, aes(x = agtronRawData$Agtron.Converted, group=1)) +
+  geom_histogram() + 
+  facet_wrap(. ~ agtronRawData$Blend) +
+  xlab("Agtron Converted Value") + ylab("Count")
+  labs(title = "Faceted Histogram of Agtron Values by Blend") 
+  
+
+# roast1OXT = rawagtronRawDataCurves[2]
 roast1BT  
 roast1AMBT
 
